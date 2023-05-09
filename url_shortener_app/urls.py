91202fb8de283fec297eb04f765c8787fd9ada77
hojongyu2 <yuhojo91@gmail.com>
2023-05-09 14:42:02 -0500

    created paths and views for encode and decode

---
+++ url_shortener_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('encode/', views.encode, name='encode'),
+    path('decode/', views.decode, name='decode'),
+]
--- url_shortener_app/views.py
+++ url_shortener_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+
+def encode(request):
+    
+    return JsonResponse({'encode': 'something'})
+
+def decode(request):
+    
+    return JsonResponse({'decode': 'something'})
