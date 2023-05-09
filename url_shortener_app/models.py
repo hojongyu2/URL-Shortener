3f987f1d907bb29a37289792aa6b0bed4d9d201b
hojongyu2 <yuhojo91@gmail.com>
2023-05-09 15:07:39 -0500

    created url model and fixed url path error. Installed psycopg as well

--- url_shortener_app/models.py
+++ url_shortener_app/models.py
@@ -1,4 +1,18 @@
 from django.db import models
+import string
+import random
 
 # Create your models here.
+class Url(models.Model):
+    original_url = models.URLField()
+    shortened_url = models.CharField(unique=True, blank=True)
+    
+    # Generate a random short URL if shortened_url field is not already set and then saves the instances
+    def save(self, *args, **kwargs):
+        if not self.shortened_url:
+            self.shortened_url = ''.join(random.choices(string.ascii_letters + string.digits, k=8)) # create random shortened url
+        super().save(*args, **kwargs) # save instance
+    
+    def __str__(self):
+        return self.original_url
 
