3f46c61abd6f49ab2be565e929c659f3437ce0e9
hojongyu2 <yuhojo91@gmail.com>
2023-05-09 16:08:02 -0500

    imported csrf_exampt and tested to grab value from POST request

--- url_shortener_app/views.py
+++ url_shortener_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
+csrf_exempt
 def encode(request):
-    
+    print(request.POST.get('url'))
     return JsonResponse({'encode': 'something'})
 
 def decode(request):
